51744347c3d95bfdf1e13a3b193d13685bf561e0
Myth <myth.mio@gmail.com>
2013-07-01 20:00:19 +0800

    砍掉重练！
    又看了一些资料，然后揣摩了一下，慢慢来不要着急

---
+++ stopWatch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  stopWatch
+//
+//  Created by Myth Myers on 13-7-1.
+//  Copyright (c) 2013年 Myth Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ stopWatch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  stopWatch
+//
+//  Created by Myth Myers on 13-7-1.
+//  Copyright (c) 2013年 Myth Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
